10ec0177bc03f4ed260fbe7b50d550ba039f966e
ProyectoIO305 <proyectoio305@gmail.com>
2025-07-02 12:15:45 -0500

    Backend Python inicial

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+# Permitir acceso desde cualquier origen (tu frontend)
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+# Estructura de datos que se espera recibir
+class Restriccion(BaseModel):
+  coef: List[float]
+  operador: str
+  valor: float
+
+class DatosProblema(BaseModel):
+  tipo: str
+  coefObjetivo: List[float]
+  restricciones: List[Restriccion]
+  esEntera: List[bool]
+
+@app.get("/")
+def inicio():
+  return {"mensaje": "Backend Python funcionando 🚀"}
+
+@app.post("/api/sensibilidad")
+async def analizar_sensibilidad(datos: DatosProblema):
+  print("Datos recibidos:", datos)
+
+  # Aquí luego programamos el análisis real
+  return {
+    "mensaje": "Datos recibidos correctamente en Python",
+    "tipo": datos.tipo,
+    "coefObjetivo": datos.coefObjetivo,
+    "restricciones": datos.restricciones,
+    "esEntera": datos.esEntera
+}
